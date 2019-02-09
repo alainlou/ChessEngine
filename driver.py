a35f835653f6cbafc7dae50e7cf56626a35adafb
alainlou <loual@utschools.ca>
2019-02-09 17:17:08 -0500

    handle position command

--- driver.py
+++ driver.py
@@ -1,3 +1,7 @@
+import chess
+
+board = chess.Board()
+
 def handleCommand(operation, parameters):
     if operation == "uci":
         print("id name BrothFish")
@@ -5,12 +9,22 @@ def handleCommand(operation, parameters):
         print("uciok")
     elif operation == "isready":
         print("readyok")
+    elif operation == "position": #set the position of the internal board
+        if parameters[:8] != "startpos":
+            fenString = parameters[:parameters.index("moves")]
+            board.set_board_fen(fenString)
+        moves = parameters[parameters.index("moves")+5:].split()
+        for move in moves:
+            foo = chess.Move.from_uci(move)
+            board.push(foo)
     elif operation == "go":
         print("bestmove h7h5")
+
+# We can make this I/O loop async eventually
 while True:
     line = input()
     if line == "quit":
         break
     else:
-        args = line.split()
-        handleCommand(args[0], args[1:])
+        flag = line.index(" ")
+        handleCommand(line[:flag], line[flag+1:])
